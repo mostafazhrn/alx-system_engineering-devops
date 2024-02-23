89736ea39064d29fc616d011cb8dadf54c3062e0
mostafazhrn <mostafazhrn@gmail.com>
2024-02-23 10:27:31 -0500

    install_pip3_flask_3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # Installs puppet-lint
 
-exec { 'install pip3':
-  command => '/usr/bin/apt-get install -y python3-pip',
-}
-
-exec { 'install flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  require => Exec['install pip3'],
-  unless  => '/usr/bin/pip3 show flask | grep -q 2.1.0',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
