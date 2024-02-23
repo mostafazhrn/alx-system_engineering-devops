80ea45dc96d987d088fefb86c5e7f5a32eb6b459
mostafazhrn <mostafazhrn@gmail.com>
2024-02-23 08:52:15 -0500

    1stCommit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# This shall create file in /tmp
+
+file { '/tmp/school':
+  ensure  => 'present',
+  content => 'I love puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
+
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+THis is a readme file for the task 0x0A-configuration_management for the alx software engineering program it consisits of 3 mandatory tasks
