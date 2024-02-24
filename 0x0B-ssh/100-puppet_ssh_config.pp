fe572df603fe52a4bd330876264cc713fe1d5530
mostafazhrn <mostafazhrn@gmail.com>
2024-02-24 14:01:47 -0500

    puppet_config1

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # chnge config for server to only connect with key no passwd
 
-file_line { 'Turn off passwd auth':
+file_line { 'Turn passwd off':
   ensure => 'present',
   path   => '/etc/ssh/sshd_config',
   line   => 'PasswordAuthentication no',
   match  => '^PasswordAuthentication.*$',
 }
 
-file_line { 'Declare identity file':
+file_line { 'Declare id file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
