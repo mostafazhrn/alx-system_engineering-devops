cf7bfce1b9906d7750e195ce8f1564c74a7aee8e
mostafazhrn <mostafazhrn@gmail.com>
2024-02-27 08:08:28 -0500

    puppet_install_nginx_fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,41 +1,26 @@
 # This script shal install nginx on server with 301 error code
 
-include stdlib
-
-$url = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
-$in = "\trewrite ^/redirect_me/$ ${url} permanent;"
-
-exec { 'update packages':
-  command => 'apt-get update',
-  path    => '/usr/bin',
-  require => Package['nginx'],
-}
-
-exec { 'restart nginx':
-  command => '/usr/sbin/service nginx restart',
-  require => Package['nginx'],
-}
-
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['update packages'],
+  ensure => 'present',
 }
 
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-  require => Package['nginx'],
+exec { 'install':
+  command  => 'sudo apt-get update && apt-get install -y nginx',
+  provider => 'shell',
+  }
+
+exec { 'Hello World':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => 'shell',
 }
 
-file_line { '301 redirect':
-  ensure   => present,
-  after    => 'server_name _;',
-  path     => '/etc/nginx/sites-available/default',
-  line     => $in,
-  multiple => true,
-  notify   => Exec['restart nginx'],
-  require  => File['/var/www/html/index.html'],
+exec { 'sudo sed -i "s/80 default_server;/80 default_server;
+  \\n\\tlocation \\/redirect_me {\\n\\t\\treturn 301 \\/;
+    \\n\\t}\\n\\t/" /etc/nginx/sites-available/default':
+  provider => 'shell',
+}
+
+exec { 'run':
+  command  => 'sudo service nginx restart',
+  provider => 'shell',
 }
