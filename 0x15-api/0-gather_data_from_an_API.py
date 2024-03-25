c9e041b1b07ce9de99bd90eec6c24b548bccf3d9
mostafazhrn <mostafazhrn@gmail.com>
2024-03-25 09:05:32 -0400

    gather_data_fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     usr = requests.get(url + "users/{}".format(sys.argv[1])).json()
     does = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
     done = [task for task in does if task.get("completed") is True]
-    print("Employee {} is done with tasks ({}/{}):".format(usr.get("name"),
-                                                           len(done),
-                                                           len(does)))
+    print("Employee {} is done with tasks({}/{}):".format(
+        usr.get("name"), len(done), len(does)))
     [print("\t {}".format(task.get("title"))) for task in done]
