df71f95d03f5f875a9cb1c9a7dd81ed46bf76b56
mostafazhrn <mostafazhrn@gmail.com>
2024-03-25 08:54:36 -0400

    readme&gather_data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""THis script shall gather data from API to return from about employees"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    usr = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    does = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    done = [task for task in does if task.get("completed") is True]
+    print("Employee {} is done with tasks ({}/{}):".format(usr.get("name"),
+                                                           len(done),
+                                                           len(does)))
+    [print("\t {}".format(task.get("title"))) for task in done]
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+This a readme file for the task 0x15-api which consists of 3 mandatory tasks as part of the alx software engineering program
+
