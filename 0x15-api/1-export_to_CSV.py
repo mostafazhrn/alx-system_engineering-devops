aef39e4a643cc4008120ad5326d3276400e36138
mostafazhrn <mostafazhrn@gmail.com>
2024-03-25 09:20:29 -0400

    export_csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""THis script shall gather data from API to return from about employees"""
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    usr = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    does = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    with open("{}.csv".format(sys.argv[1]), "w") as csvfile:
+        write = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [write.writerow([usr.get("id"), usr.get("username"),
+                         task.get("completed"),
+                         task.get("title")]) for task in does]
