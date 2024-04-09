d264f590f73eca5693215309b45d1ad7c4582a60
mostafazhrn <mostafazhrn@gmail.com>
2024-04-09 14:38:16 -0400

    fix_subs_1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-""" THis script shall count the number of subscribers for given subreddit """
+"""
+THis script shall count the number of subscribers for given subreddit to return
+the num of total subs
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ THis shall query the Reddit API and return number of subscribers"""
+    """
+    THis shall query the Reddit API and return number of subscribers or to
+    give zero if fake sub reddit
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     hdrs = {'User-Agent': 'Mozilla/5.0'}
     resp = requests.get(url, headers=hdrs, allow_redirects=False)
