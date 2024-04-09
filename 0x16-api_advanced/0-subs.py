766f0f8c494fe464968a1160b25c44426a63437c
mostafazhrn <mostafazhrn@gmail.com>
2024-04-09 15:52:56 -0400

    fix_sub_again

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,10 +12,11 @@ def number_of_subscribers(subreddit):
     THis shall query the Reddit API and return number of subscribers or to
     give zero if fake sub reddit
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    hdrs = {'User-Agent': 'Mozilla/5.0'}
-    resp = requests.get(url, headers=hdrs, allow_redirects=False)
-    if resp.status_code != 200:
+    bz_url = 'https://www.reddit.com'
+    url = '{base}/r/{subreddit}/about.json'.format(base=bz_url,
+                                                   subreddit=subreddit)
+    usr_agt = {'User-Agent': 'Python/requests'}
+    respo = requests.get(url, headers=usr_agt, allow_redirects=False)
+    if respo.status_code != 200:
         return 0
-    compte_subs = resp.json().get('data').get('subscribers')
-    return compte_subs
+    return respo.json().get('data').get('subscribers')
