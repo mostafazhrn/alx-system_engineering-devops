9c65823746c9c3338ce8446f55227d076296dcbb
mostafazhrn <mostafazhrn@gmail.com>
2024-04-11 07:10:51 -0400

    fix_puppet file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
-# THis script shall fix typos in wordpress file
+#THis script shall fix typos in wordpress file
 
-exec { 'fix_wordpress_typo':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path    => '/usr/bin:/bin',
-    require => File['/var/www/html/wp-settings.php'],
+exec { 'fix_typo':
+  path     => ['/bin', '/usr/bin', '/usr/sbin', '/sbin'],
+  command  => 'sed -i s/.phpphp/.php/g /var/www/html/wp-settings.php',
+  provider => 'shell',
 }
